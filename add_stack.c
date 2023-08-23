68657a9fbadfd0ea3ee8d62ae10baf4d3e8fb741
aicha652 <aichaazlf652@gmail.com>
2023-08-23 15:13:56 -0700

    task 4

--- add_stack.c
+++ add_stack.c
@@ -7,24 +7,18 @@
  */
 void add_stack(stack_t **stack, unsigned int line_number)
 {
-	int count = 0, sum = 0;
-	stack_t *tmp, *link;
+	stack_t *tmp;
 
-	link = *stack;
-	while (link != NULL)
-	{
-		count++;
-		link = link->next;
-	}
-	if (count < 2)
+	(void)line_number;
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	sum = (*stack)->n + (*stack)->next->n;
+	(*stack)->n += (*stack)->next->n;
 	tmp = *stack;
 	*stack = (*stack)->next;
-	(*stack)->n = sum;
+	(*stack)->n = tmp->n;
 	(*stack)->prev = NULL;
 	free(tmp);
 }
