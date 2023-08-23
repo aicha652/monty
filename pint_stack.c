3b7bf0b4f892bde5b740ed0f38e5ac8fe40beec1
aicha652 <aichaazlf652@gmail.com>
2023-08-23 10:51:38 -0700

    task 1

--- pint_stack.c
+++ pint_stack.c
@@ -8,13 +8,14 @@
  */
 void pint_stack(stack_t **stack, unsigned int line_number)
 {
-	if (*stack != NULL)
+	/*if (*stack != NULL)
 	{
 		printf("%d\n", (*stack)->n);
-	}
-	else
+	}*/
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	printf("%d\n", (*stack)->n);
 }
